1f6f8baae897d062e4bc4f8ff500bf39cbb719a0
Hope <roni.trololo2004@gmail.com>
2024-08-18 16:14:24 +0300

    Dynamic Table

--- Site.js
+++ Site.js
@@ -1,11 +1,10 @@
 // Sample data
+
 const data = [
-    ["Name", "Age", "Profession"],
-    ["John Doe", 30, "Developer"],
-    ["Jane Smith", 25, "Designer"],
-    ["Sam Brown", 35, "Manager"],
-    ["Alice Green", 28, "Engineer"],
-    ["Bob White", 40, "Architect"]
+    ['./resourses/ace_of_clubs.png', './resourses/ace_of_diamonds.png', "./resourses/ace_of_hearts.png", "./resourses/ace_of_spades.png"],
+    ['./resourses/jack_of_clubs2.png', "./resourses/jack_of_diamonds2.png", "./resourses/jack_of_hearts2.png", "./resourses/jack_of_spades2.png"],
+    ['./resourses/king_of_clubs2.png', "./resourses/king_of_diamonds2.png", "./resourses/king_of_hearts2.png", "./resourses/king_of_spades2.png"],
+    ['./resourses/queen_of_clubs2.png', "./resourses/queen_of_diamonds2.png", "./resourses/queen_of_hearts2.png", "./resourses/queen_of_spades2.png"],
 ];
 
 function generateResponsiveTable(data) {
@@ -13,12 +12,14 @@ function generateResponsiveTable(data) {
     
     data.forEach((row, rowIndex) => {
         row.forEach((cellContent) => {
+            console.log(cellContent);
             const cell = document.createElement('div');
-            cell.className = rowIndex === 0 ? 'grid-table-header' : 'grid-table-cell';
-            cell.innerText = cellContent;
+            const newElement = document.createElement('img');
+            newElement.setAttribute('class', "grid-table-cell");
+            newElement.setAttribute('src', cellContent);
+            cell.appendChild(newElement);
             table.appendChild(cell);
         });
     });
 }
 
-generateResponsiveTable(data);
